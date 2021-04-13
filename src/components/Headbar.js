361aae37a4e1bf37991850953191cca5f4b5cd8a
woolosh <joshuapmorales@gmail.com>
2021-04-12 22:09:34 -0500

    initial commit -frontend

---
+++ src/components/Headbar.js
@@ -0,0 +1,12 @@
+const Headbar = () => {
+  return (
+    <div className="jumbotron bg-secondary text-center text-danger">
+      <div className="container">
+        <h1>New Arrival</h1>
+        <h4>Pregnancy Assistance</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Headbar
